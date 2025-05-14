05f8c44207c6b29819f7407c57cdbff077ac4839
denissspipi <hazbinn@mail.ru>
2025-05-14 14:45:11 +0300

    add

--- server/server.js
+++ server/server.js
@@ -19,10 +19,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client')));
 
-// Подключение к MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('MongoDB connection error:', err));
 
 // API маршруты
 app.use('/api/auth', authRoutes);
